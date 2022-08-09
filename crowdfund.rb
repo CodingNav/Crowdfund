0486f08fed96b2c4358b2c76881e669a4438b633
Navleen Kaur <navleenkaur@Navleens-MacBook-Pro.local>
2022-08-09 17:36:59 -0400

    Create Project class with add and remove fund methods

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,7 +1,40 @@
-def project_listing (name, fund=0)
-    "#{name.ljust(30, '.')} $#{fund} in funding."
+# def project_listing (name, fund=0)
+#     "#{name.ljust(30, '.')} $#{fund} in funding."
+# end
+
+# puts project_listing("Project ABC", 1000)
+# puts project_listing("Project LMN", 500)
+# puts project_listing("Project XYZ", 25)
+
+class Project
+    
+    def initialize(name, fund, target_fund)
+        @name = name
+        @fund = fund
+        @target_fund = target_fund
+    end
+    def to_s
+        "#{@name} has $#{@fund} in funding towards a goal of $#{@target_fund}."
+    end
+    def add_funds
+        @fund += 25
+        puts "#{@name} got more funds!"
+    end
+    def remove_funds
+        @fund -=15
+        puts "#{@name} lost some funds!"
+    end
+
 end
 
-puts project_listing("Project ABC", 1000)
-puts project_listing("Project LMN", 500)
-puts project_listing("Project XYZ", 25)
+project1 = Project.new("Project LMN", 500, 3000)
+project2 = Project.new("Project XYZ", 25, 75)
+
+puts project1
+puts project2
+
+project1.remove_funds
+project2.add_funds
+
+puts project1
+puts project2
