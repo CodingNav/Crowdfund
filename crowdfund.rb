dde0c350b781f52195c1172076a956a77c0ccdb3
Navleen Kaur <navleenkaur@Navleens-MacBook-Pro.local>
2022-08-10 10:41:37 -0400

    Created projects array and iterators

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,11 +1,3 @@
-# def project_listing (name, fund=0)
-#     "#{name.ljust(30, '.')} $#{fund} in funding."
-# end
-
-# puts project_listing("Project ABC", 1000)
-# puts project_listing("Project LMN", 500)
-# puts project_listing("Project XYZ", 25)
-
 class Project
     
     attr_accessor :name
@@ -35,30 +27,36 @@ class Project
 end
   
 project1 = Project.new("Project ABC", 5000, 1000)
-puts project1
-puts project1.funding_needed
-
 project2 = Project.new("Project LMN", 3000, 500)
-puts project2
-
 project3 = Project.new("Project XYZ", 75, 25)
-puts project3
-
 project4 = Project.new("Project TBD", 10000)
-puts project4
 
-puts "***"
-puts project4.name
-project4.name = "Project123"
-puts project4.name
-project1.remove_funds
-project2.remove_funds
-project3.add_funds
-project4.add_funds
-puts "***"
+projects = [project1, project2, project3, project4]
 
-puts project1
-puts project1.funding_needed
-puts project2
-puts project3
-puts project4
+puts "There are #{projects.size} projects that you could fund:"
+projects.each do |project|
+    puts project
+end
+
+puts "Here are the target funding amounts of each project:"
+projects.each do |project|
+    puts project.target_fund
+end
+
+puts "______________________"
+
+puts "Let's go through a round of funding requests and see what happens:"
+projects.each do |project|
+    project.add_funds
+    project.add_funds
+    project.remove_funds
+    puts project
+end
+
+puts "______________________"
+
+puts "Project ABC is removed and Project QRS is added. Now we have:"
+projects.delete(project1)
+project5 = Project.new("Project QRS", 300, 500)
+projects.push(project5)
+puts projects
